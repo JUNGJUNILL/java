d0660228e6cf429ba5c56c5e3a709476a9f09279
정준일 <jijeong@kbrainc.com>
2024-01-30 20:43:46 +0900

    commit

--- HelloJava/src/helloJava/kbrain/P002_Logic.java
+++ HelloJava/src/helloJava/kbrain/P002_Logic.java
@@ -5,6 +5,7 @@ public class P002_Logic {
     public static void main(String[] args) {
         
         System.out.println("test logic");
+        System.out.println("test logic test");
     }
     
 }
